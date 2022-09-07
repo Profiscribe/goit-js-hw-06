6acd7ff8bf7dfd8de49f5776330d1754eaed68fa
Оксана Качмар <108275501+Profiscribe@users.noreply.github.com>
2022-09-07 21:18:55 +0300

    Поправила дз

--- js/task-06.js
+++ js/task-06.js
@@ -15,6 +15,7 @@
 // Для додавання стилів використовуй CSS-класи valid і invalid, які ми вже додали у вихідні файли завдання.
 
 
+const textInput = document.querySelector('#validation-input')
 console.log(textInput.getAttribute('data-length'))
 textInput.addEventListener('blur', event => {
 	if (event.target.value.length == textInput.getAttribute('data-length')) {
--- js/task-08.js
+++ js/task-08.js
@@ -28,12 +28,12 @@ function handleSubmit(event) {
     event.preventDefault()
     const { email, password } = event.currentTarget.elements;
 
-    if (email.value === '' || password.value === '') {
+    if (email.value.trim() === '' || password.value.trim() === '') {
         return alert('Please, fill in all the fields!')
     }
     const userDetails = {
-        email: email.value,
-        Password: password.value
+        email: email.value.trim()
+        Password: password.value.trim()
     }
     
     console.log(userDetails)
